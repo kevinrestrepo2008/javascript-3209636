deab34bcefc06a17b886eb9d302f349c44d77c61
kevinrestrepo2008 <rrkevin711@gmail.com>
2025-10-15 15:30:17 -0500

    avance de color en html, css, js

---
+++ 1-escenario-interactivo/app.js
@@ -0,0 +1,14 @@
+const section = document.querySelector('button');
+const button = document.querySelector('button');
+
+if (section && button) {
+button.addEventListener('click', function() {
+
+
+    section.style.backgroundColor = 'blue';
+
+})
+
+
+
+}
