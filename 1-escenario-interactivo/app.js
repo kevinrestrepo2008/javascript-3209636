bf697fa4fce049420db1dc307305d8c5298e82e0
kevinrestrepo2008 <rrkevin711@gmail.com>
2025-10-24 14:49:01 -0500

    los 3 escenarios de juegos  ya con css y js

--- 1-escenario-interactivo/app.js
+++ 1-escenario-interactivo/app.js
@@ -29,3 +29,72 @@ item.addEventListener("animationend", () => {
 
 
 })
+
+/* juego2 */
+
+const pac = document.querySelectorAll('.pac');
+const fantasma = document.querySelector('.fantasma');
+const score3 = document.querySelector('.score3');
+let l = 0;
+
+
+console.log(pac);
+console.log(fantasma);
+
+
+pac.forEach(item => {
+
+  /*   console.log("elemento: ", item); */
+    
+
+item.addEventListener("click", () => {
+item.classList.add("saltar")
+
+l++
+score3.textContent = `score = ${l}`
+
+
+})
+item.addEventListener("animationend", () => {
+    item.style.display = "none"
+}, {once: true})
+
+
+
+
+})
+
+
+/* juego3 */
+
+const angry = document.querySelectorAll('.angry');
+const pajaro = document.querySelector('.pajaro');
+const score4 = document.querySelector('.score4');
+let k = 0;
+
+
+console.log(angry);
+console.log(pajaro);
+
+
+angry.forEach(item => {
+
+  /*   console.log("elemento: ", item); */
+    
+
+item.addEventListener("click", () => {
+item.classList.add("saltar")
+
+k++
+score4.textContent = `score = ${k}`
+
+
+})
+item.addEventListener("animationend", () => {
+    item.style.display = "none"
+}, {once: true})
+
+
+
+
+})
