345511572584cfe9f1d09f49124b14268fd47c24
kevinrestrepo2008 <rrkevin711@gmail.com>
2025-11-06 15:21:41 -0500

    Add files via upload

---
+++ 2-base-datos-comic/bd.js
@@ -0,0 +1,39 @@
+let comic = {
+  "nombreComic": "Piktara",
+  "sinopsis": "Un comic que narra la antiguedad y la invencion de la escritura",
+  "year": 2025,
+  "autores": ["kevin","Melany"],
+  "personajes": ["Ninove","Adad","Naramsin"],
+  "genero": "Historia",
+  "portada": "./images/portada.jpg",
+  "escenas": [
+    {
+      "name": "Molecule Man",
+      "age": 29,
+      "secretIdentity": "Dan Jukes",
+      "powers": ["Radiation resistance", "Turning tiny", "Radiation blast"]
+    },
+    {
+      "name": "Madame Uppercut",
+      "age": 39,
+      "secretIdentity": "Jane Wilson",
+      "powers": [
+        "Million tonne punch",
+        "Damage resistance",
+        "Superhuman reflexes"
+      ]
+    },
+    {
+      "name": "Eternal Flame",
+      "age": 1000000,
+      "secretIdentity": "Unknown",
+      "powers": [
+        "Immortality",
+        "Heat Immunity",
+        "Inferno",
+        "Teleportation",
+        "Interdimensional travel"
+      ]
+    }
+  ]
+}
