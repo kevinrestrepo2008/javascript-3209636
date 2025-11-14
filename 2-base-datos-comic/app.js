b46d2416b7ba63d1e1a36ecf4898a0aca9ad252e
kevinrestrepo2008 <rrkevin711@gmail.com>
2025-11-14 15:35:47 -0500

    avance js

---
+++ 2-base-datos-comic/app.js
@@ -0,0 +1,48 @@
+const hero  = document.querySelector (".hero")
+const content = document.querySelector(".hero-content")
+
+console.log (hero)
+console.log (hero-content)
+
+hero.innerHTML = `
+<h1>${}
+
+`
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* const infoComic = document.querySelector(".info-comic")
+const listaCapitulos = document.querySelector(".lista-capitulos")
+
+console.log (infoComic)
+console.log (listaCapitulos)
+
+//Modificar el contenido general del comic//
+
+infoComic.innerHTML = `
+<p>$()
+` */
